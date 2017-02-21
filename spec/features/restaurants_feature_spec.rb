0a82a8145c8b3c784f9cd079812e179673418944
konradschlatte <konrad.schlatte@btinternet.com>
2017-02-21 15:21:44 +0000

    section 10

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -9,6 +9,7 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
   context 'restaurants have been added' do
     before do
       Restaurant.create(name: 'KFC')
@@ -43,7 +44,23 @@ feature 'restaurants' do
      expect(page).to have_content 'KFC'
      expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
-
   end
+
+  context 'editing restaurants' do
+
+  before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+  scenario 'let a user edit a restaurant' do
+    visit '/restaurants'
+    click_link 'Edit KFC'
+    fill_in 'Name', with: 'Kentucky Fried Chicken'
+    fill_in 'Description', with: 'Deep fried goodness'
+    click_button 'Update Restaurant'
+    click_link 'Kentucky Fried Chicken'
+    expect(page).to have_content 'Kentucky Fried Chicken'
+    expect(page).to have_content 'Deep fried goodness'
+    expect(current_path).to eq '/restaurants/1'
+  end
+
+end
 
 end
