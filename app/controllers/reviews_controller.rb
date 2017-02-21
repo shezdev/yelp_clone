9efaff5fb636e8411186505e9ad780943c59af75
konradschlatte <konrad.schlatte@btinternet.com>
2017-02-21 20:55:11 +0000

    section 12

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,10 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @restaurant.reviews.create(review_params)
-    redirect_to '/restaurants'
+    path = "/restaurants/#{@restaurant.id}"
+    redirect_to path
+
+
   end
 
 private
