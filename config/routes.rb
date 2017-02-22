7d42fc50db1e99f710666986e53a7e0ad8c585bf
Shereen Finney <shereenfinney@gmail.com>
2017-02-22 16:53:01 +0000

    Attempting facebook integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 
 
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   root "restaurants#index"
-  devise_for :users
+
   resources :restaurants do
     resources :reviews
   end
